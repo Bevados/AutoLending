92fcf5fba6ecbcfb97f2b8e2b4efd4b98770d50d
“Vladislav <boromirchik@gmail.com>
2022-04-30 18:42:16 +0300

    Block why done

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,38 @@
 //Функция для добавления класса webp или no-webp для html
 import * as webpClass from "./modules/webp.js";
 webpClass.isWebp();
+
+
+
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+   window.addEventListener('scroll', animOnScroll);
+   function animOnScroll() {
+      for (let i = 0; i < animItems.length; i++) {
+         const animItem = animItems[i];
+         const animItemHeight = animItem.offsetHeight;
+         const animItemOffset = offset(animItem).top;
+         const animStart = 4;
+
+         let animItemPoint = window.innerHeight - animItemHeight / animStart;
+         if (animItemHeight > window.innerHeight) {
+            animItemPoint = window.innerHeight - window.innerHeight / animStart;
+         }
+
+         if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemPoint)) {
+            animItem.classList.add('_active');
+         }
+      }
+   }
+
+   function offset(el) {
+      const rect = el.getBoundingClientRect(),
+         scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      return {top: rect.top + scrollTop}
+   }
+   animOnScroll();
+}
+
+
+
