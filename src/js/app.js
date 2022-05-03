81964e5a7651ad89182e15d1048d8fae8453e427
“Vladislav <boromirchik@gmail.com>
2022-05-03 12:28:10 +0300

    modal window js done

--- src/js/app.js
+++ src/js/app.js
@@ -3,36 +3,22 @@ import * as webpClass from "./modules/webp.js";
 webpClass.isWebp();
 
 
-
-const animItems = document.querySelectorAll('._anim-items');
+// Функция анимации
+import {animItems, animOnScroll} from "./modules/animation.js";
 
 if (animItems.length > 0) {
-   window.addEventListener('scroll', animOnScroll);
-   function animOnScroll() {
-      for (let i = 0; i < animItems.length; i++) {
-         const animItem = animItems[i];
-         const animItemHeight = animItem.offsetHeight;
-         const animItemOffset = offset(animItem).top;
-         const animStart = 4;
-
-         let animItemPoint = window.innerHeight - animItemHeight / animStart;
-         if (animItemHeight > window.innerHeight) {
-            animItemPoint = window.innerHeight - window.innerHeight / animStart;
-         }
-
-         if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemPoint)) {
-            animItem.classList.add('_active');
-         }
-      }
-   }
-
-   function offset(el) {
-      const rect = el.getBoundingClientRect(),
-         scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-      return {top: rect.top + scrollTop}
-   }
    animOnScroll();
+   window.addEventListener('scroll', animOnScroll);
 }
+
+
+//Открытие закрытие модального окна
+import {openDownload} from "./modules/openCloseDownload.js";
+window.addEventListener('click', openDownload);
+
+
+
+
 
 
 
